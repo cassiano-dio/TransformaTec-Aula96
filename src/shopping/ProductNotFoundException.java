2a5603dc83a5a91b89e6d3ed0a27ea70991ef235
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-07-21 13:27:09 -0300

    Update ProductNotFoundException.java

--- src/shopping/ProductNotFoundException.java
+++ src/shopping/ProductNotFoundException.java
@@ -1,5 +1,7 @@
 package shopping;
 
+//criando uma classe de erro personalizado do tipo unchecked - tempo de execução
+//herança da classe pai
 public class ProductNotFoundException extends Exception{
 
     public ProductNotFoundException(String message) {
