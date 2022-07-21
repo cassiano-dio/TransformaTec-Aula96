ae34eee3ffc83de94a09beebbfdc7826545f33e4
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-07-21 13:26:09 -0300

    Update OptionException.java

--- src/shopping/OptionException.java
+++ src/shopping/OptionException.java
@@ -1,5 +1,7 @@
 package shopping;
 
+//criando uma classe de erro personalizado do tipo unchecked - tempo de execução
+//herança da classe pai
 public class OptionException extends Exception{
 
     public OptionException(String message){
